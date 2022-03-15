759f2b2341b9252404d40970f0a2f34844b1e0d9
DarkLorian <66025673+DarkLorianPrime@users.noreply.github.com>
2022-03-15 21:29:19 +0400

    Update settings.py

--- vk_callback/settings.py
+++ vk_callback/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n#9lu)&y#z6!6-y^kukucd1xup7v(e!bchy(okuo-!!%fnjkje'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
